d811e0c4dc7f575a5cb79342c39ff40e4349d116
Kevin Poljans <kevinpoljans@hotmail.com>
2025-11-27 19:45:47 +0200

    completed 07-js and o8-js

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+/**
+ * 
+ * @param {string} string 
+ */
 
+const reverseString = function(string) {
+    let reversedString = "";
+    
+    for(let i = string.length - 1; i >= 0; i--){
+        reversedString += string.at(i);
+    }
+
+    return reversedString;
 };
 
 // Do not edit below this line
--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+/**
+ * 
+ * @param {Array} array 
+ * @param {number} num 
+ */
+
+
+
+const removeFromArray = function(array, ...items) {
+    let newArray = array;
+    for(const item of items){
+        newArray = newArray.filter((i) => i !== item);
+    }
+    return newArray;
 };
 
 // Do not edit below this line
