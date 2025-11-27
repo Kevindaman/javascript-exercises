8ae6432e80789121b5ac5cb77c4cfad1c019d542
Kevin Poljans <kevinpoljans@hotmail.com>
2025-11-27 19:14:29 +0200

    completed 06-js

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,21 @@
-const repeatString = function() {
+/**
+ * 
+ * @param {string} string 
+ * @param {number} num 
+ */
 
+const repeatString = function(string, num) {
+    if (num < 0){
+        return "ERROR";
+    }
+
+    let fullWord = "";
+
+    for (let i = 0; i < num; i++){
+        fullWord = fullWord + string;
+    }
+
+    return fullWord;
 };
 
 // Do not edit below this line
