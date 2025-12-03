c2e83069693178b903735bb2d00f80e01f1bb84e
Kevin Poljans <kevinpoljans@hotmail.com>
2025-12-03 13:40:32 +0200

    completed 13-js

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+/**
+ * 
+ * @param {string} string 
+ */
 
+
+const palindromes = function (string) {
+    let reversedString = string.split("").filter(char => {
+        if (char == "!" || char == "?" || char =="." || char == "," || char == " ") return false;
+        else return true;
+    }).reverse().join("").toLowerCase();
+
+   
+    string = string.replaceAll(/[!?.,]/g, "").replaceAll(" ", "").toLowerCase();
+    
+    return string === reversedString;
 };
 
 // Do not edit below this line
