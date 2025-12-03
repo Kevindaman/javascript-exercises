be886ef9f6b6ba158704031923cf7873776b52a9
Kevin Poljans <kevinpoljans@hotmail.com>
2025-12-03 13:03:36 +0200

    completed 12_js

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
+};
+/**
+ * 
+ * @param {Array} arr 
+ */
+
+const sum = function(arr) {
+	let sum = 0;
+  arr.forEach(num => sum += num)
+  return sum;
 };
 
-const sum = function() {
-	
+const multiply = function(arr) {
+  let sum = 1;
+  arr.forEach(num => sum *= num)
+  return sum;
 };
 
-const multiply = function() {
-
+const power = function(base, exp) {
+	return base**exp;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+	let sum = 1;
+  for(let i = num; i > 0; i--){
+    sum *= i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
