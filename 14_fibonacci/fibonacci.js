84aa8af644359444367b0c1de03756ee4bc743a1
Kevin Poljans <kevinpoljans@hotmail.com>
2025-12-03 13:52:00 +0200

    completed 14-js

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num == 0){
+        return 0;
+    }
+    if (num < 0){
+        return "OOPS";
+    }
+    let a = 1;
+    let b = 1;
+    if(num == 1 || num == 2) return 1;
+    else{
+        num -= 2;
+        while(num > 0){
+            let temp = b;
+            b = b + a;
+            a = temp;
+            num--;
+        }
+        return b;
+    }
 };
 
 // Do not edit below this line
