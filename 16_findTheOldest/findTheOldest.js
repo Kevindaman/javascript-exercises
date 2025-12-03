0863258e062af53e812362dc1cdb2306fd7ce4aa
Kevin Poljans <kevinpoljans@hotmail.com>
2025-12-03 14:13:21 +0200

    completed 16-js

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let oldestPerson;
+    let oldestAge = 0;
 
+    for (let person of arr){
+        let age;
+        if(!("yearOfDeath" in person)){
+            let currentYear = new Date().getFullYear()
+            age = currentYear - person.yearOfBirth;
+        }
+        else{
+            age = person.yearOfDeath - person.yearOfBirth;
+        }
+        if(age > oldestAge){
+            oldestAge = age;
+            oldestPerson = person;
+        }
+    }
+    return oldestPerson;
 };
 
 // Do not edit below this line
